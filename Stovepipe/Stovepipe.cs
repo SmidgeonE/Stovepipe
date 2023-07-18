31906987309e62975ff9527e599d03cdbe1b8e0d
Smidgeon <finlaynugent@gmail.com>
2023-07-18 15:41:20 +0100

    Refactored old code

--- Stovepipe/Stovepipe.cs
+++ Stovepipe/Stovepipe.cs
@@ -14,25 +14,22 @@ namespace Stovepipe
     [BepInProcess("h3vr.exe")]
     public class EjectionFailure : BaseUnityPlugin
     {
-        private static FVRFireArmRound EjectedRound;
-        private static Rigidbody EjectedRoundRb;
-        private static Transform EjectedRoundTransform;
-        private static int RoundDefaultLayer;
-        private static float EjectedRoundWidth;
-        private static float DefaultFrontPosition;
+        private static FVRFireArmRound _ejectedRound;
+        private static Rigidbody _ejectedRoundRb;
+        private static Transform _ejectedRoundTransform;
+        private static int _roundDefaultLayer;
+        private static float _ejectedRoundWidth;
+        private static float _defaultFrontPosition;
         
-        
-        private const float stovepipeProb = 0.5f;
-        private static bool isStovepiping;
-        private static bool isClippingThroughbullet;
-        private static bool hasBulletBeenSetNonColliding;
-        private static bool hasBeenRotatedUpwards;
+        private const float StovepipeProb = 0.5f;
+        private static bool _isStovepiping;
+        private static bool _hasBulletBeenSetNonColliding;
 
-        private static bool hasCollectedDefaultFrontPosition;
+        private static bool _hasCollectedDefaultFrontPosition;
+        private static CapsuleCollider _bulletCollider;
 
         private void Awake()
         {
-            Debug.Log("Failure to eject");
             Harmony.CreateAndPatchAll(typeof(EjectionFailure), null);
         }
 
@@ -44,7 +41,7 @@ namespace Stovepipe
 
             var handgunTransform = __instance.transform;
             
-            EjectedRound = __instance.Chamber.EjectRound(__instance.RoundPos_Ejection.position, 
+            _ejectedRound = __instance.Chamber.EjectRound(__instance.RoundPos_Ejection.position, 
                 handgunTransform.right * __instance.RoundEjectionSpeed.x 
                 + handgunTransform.up * __instance.RoundEjectionSpeed.y 
                 + handgunTransform.forward * __instance.RoundEjectionSpeed.z, 
@@ -54,33 +51,30 @@ namespace Stovepipe
                 __instance.RoundPos_Ejection.position, __instance.RoundPos_Ejection.rotation, 
                 false);
 
-            if (EjectedRound == null)
+            if (_ejectedRound == null)
             {
                 Debug.Log("Ejected round is null");
                 return false;
             }
             
-            EjectedRoundRb = EjectedRound.GetComponent<Rigidbody>();
-            EjectedRoundTransform = EjectedRound.transform;
-            
-            
-            var bulletCollider = EjectedRound.GetComponent<CapsuleCollider>();
+            _ejectedRoundRb = _ejectedRound.GetComponent<Rigidbody>();
+            _ejectedRoundTransform = _ejectedRound.transform;
+            _bulletCollider = _ejectedRound.GetComponent<CapsuleCollider>();
 
-            if (bulletCollider is null)
+            if (_bulletCollider is null)
             {
                 Debug.Log("bullet has no collider mesh");
                 return false;
             }
 
-            if (!EjectedRound.IsSpent)
+            if (!_ejectedRound.IsSpent)
             {
-                Debug.Log("No longer stovepipping");
-                isStovepiping = false;
+                _isStovepiping = false;
             }
 
-            EjectedRoundWidth = bulletCollider.bounds.size.y;
+            _ejectedRoundWidth = _bulletCollider.bounds.size.y;
             
-            Debug.Log("eject round has width:" + EjectedRoundWidth);
+            Debug.Log("eject round has width:" + _ejectedRoundWidth);
 
             return false;
         }
@@ -91,10 +85,8 @@ namespace Stovepipe
         {
             if (__instance.IsHeld) return;
             if (!__instance.Handgun.Chamber.IsFull) return;
-            isStovepiping = Random.Range(0f, 1f) < stovepipeProb;
-            hasBulletBeenSetNonColliding = false;
-
-            if (isStovepiping) Debug.Log("Stovepiping...");
+            _isStovepiping = Random.Range(0f, 1f) < StovepipeProb;
+            _hasBulletBeenSetNonColliding = false;
         }
 
 
@@ -102,131 +94,87 @@ namespace Stovepipe
         [HarmonyPrefix]
         private static void SlidePatch(HandgunSlide __instance, ref float ___m_slideZ_forward, ref float ___m_slideZ_current)
         {
-            if (!hasCollectedDefaultFrontPosition)
+            if (!_hasCollectedDefaultFrontPosition)
             {
                 Debug.Log("collecting default position");
-                DefaultFrontPosition = ___m_slideZ_forward;
-                hasCollectedDefaultFrontPosition = true;
+                _defaultFrontPosition = ___m_slideZ_forward;
+                _hasCollectedDefaultFrontPosition = true;
             }
 
-            if (!isStovepiping)
+            if (!_isStovepiping)
             {
-                ___m_slideZ_forward = DefaultFrontPosition;
+                ___m_slideZ_forward = _defaultFrontPosition;
                 return;
             }
             
-
-            if (!hasBulletBeenSetNonColliding)
+            if (!_hasBulletBeenSetNonColliding)
             {
                 Debug.Log("forward float:" + ___m_slideZ_forward);
-                Debug.Log("Ejected round width float: " + EjectedRoundWidth);
-                Debug.Log("forward position limit " + (DefaultFrontPosition - EjectedRoundWidth));
+                Debug.Log("Ejected round width float: " + _ejectedRoundWidth);
+                Debug.Log("forward position limit " + (_defaultFrontPosition - _ejectedRoundWidth));
             }
             
-            var forwardPositionLimit = DefaultFrontPosition - EjectedRoundWidth;
+            var forwardPositionLimit = _defaultFrontPosition - _ejectedRoundWidth;
             ___m_slideZ_forward = forwardPositionLimit;
-
-            /*
-            if (___m_slideZ_current < forwardPositionLimit) ___m_slideZ_current = forwardPositionLimit;
-            */
-
+            
             if (__instance.IsHeld)
             {
-                Debug.Log("it is held, stopping stovepipe");
-                isStovepiping = false;
+                _isStovepiping = false;
                 return;
             }
 
             /* Stovepipe the round...
              */
             
-            if (!hasBulletBeenSetNonColliding) SetBulletToNonColliding(__instance);
+            if (!_hasBulletBeenSetNonColliding) SetBulletToStovepiping(__instance);
 
+            var slideTransform = __instance.transform;
             
-            EjectedRound.RootRigidbody.position = __instance.transform.position + 0.1f * __instance.transform.up.normalized;
-            
-            
-            Debug.Log("Applying roaetatino");
-            /*
-            EjectedRound.RootRigidbody.rotation = __instance.transform.rotation;
-            */
-            
-            EjectedRound.RootRigidbody.rotation = Quaternion.LookRotation(__instance.transform.up, -__instance.transform.forward);
-
-        }
-
-        /*
-        [HarmonyPatch(typeof(HandgunSlide), "UpdateSlide")]
-        [HarmonyPostfix]
-        private static void ForceSlideToFrontPatch(float __state)
-        {
-            if (!isStovepiping) return;
-            
-            Debug.Log("Forcing the slide to the front with state value, " + __state);
-            
+            _ejectedRound.RootRigidbody.position = new Vector3(slideTransform.position.x, slideTransform.position.y, __instance.Handgun.RoundPos_Ejection.position.z);
+            _ejectedRound.RootRigidbody.rotation = Quaternion.LookRotation(slideTransform.up, -slideTransform.forward);
             
         }
-        */
 
 
         [HarmonyPatch(typeof(FVRFireArmRound), "BeginAnimationFrom")]
         [HarmonyPrefix]
         private static bool CancelAnimationPatch(bool ___m_canAnimate)
         {
-            return !isStovepiping;
+            return !_isStovepiping;
         }
-
-
-
-        private static void SetBulletToNonColliding(HandgunSlide slide)
+        
+        private static void SetBulletToStovepiping(HandgunSlide slide)
         {
             Debug.Log("setting bullet to non colliding");
 
-            RoundDefaultLayer = EjectedRound.gameObject.layer;
+            _roundDefaultLayer = _ejectedRound.gameObject.layer;
             
-            EjectedRound.gameObject.layer = LayerMask.NameToLayer("Interactable");
-            EjectedRound.RootRigidbody.velocity = Vector3.zero;
-            EjectedRound.RootRigidbody.angularVelocity = Vector3.zero;
-            EjectedRound.RootRigidbody.maxAngularVelocity = 0;
-            EjectedRound.RootRigidbody.useGravity = false;
-            EjectedRound.IsDestroyedAfterCounter = false;
+            _ejectedRound.gameObject.layer = LayerMask.NameToLayer("Interactable");
+            _ejectedRound.RootRigidbody.velocity = Vector3.zero;
+            _ejectedRound.RootRigidbody.angularVelocity = Vector3.zero;
+            _ejectedRound.RootRigidbody.maxAngularVelocity = 0;
+            _ejectedRound.RootRigidbody.useGravity = false;
 
-
-            EjectedRoundTransform.position = slide.Point_Slide_Forward.position;
-            hasBulletBeenSetNonColliding = true;
+            _ejectedRoundTransform.position = slide.Point_Slide_Forward.position;
+            _hasBulletBeenSetNonColliding = true;
+            _bulletCollider.isTrigger = true;
         }
 
         private static void SetBulletBackToNormal()
         {
             Debug.Log("Setting bullet back to normal.");
-            EjectedRound.RootRigidbody.useGravity = true;
-            hasBulletBeenSetNonColliding = false;
-            isStovepiping = false;
-            EjectedRound.gameObject.layer = RoundDefaultLayer;
-            EjectedRound.IsDestroyedAfterCounter = false;
-            hasBeenRotatedUpwards = false;
+            _ejectedRound.RootRigidbody.useGravity = true;
+            _hasBulletBeenSetNonColliding = false;
+            _isStovepiping = false;
+            _ejectedRound.gameObject.layer = _roundDefaultLayer;
+            _bulletCollider.isTrigger = false;
         }
-        
-        
-        /*
-        [HarmonyPatch(typeof(HandgunSlide), "UpdateSlide")]
-        [HarmonyPostfix]
-        private static void SlidePostfix(ref float ___m_slideZ_forward,
-            ref float ___m_slideZ_current)
-        {
-            if (isStovepiping && isClippingThroughbullet)
-            {
-                Debug.Log("Is clipping through the bullet, returning to end of bullet");
-                ___m_slideZ_current = (___m_slideZ_forward - EjectedRoundWidth) * 0.9f;
-                isClippingThroughbullet = false;
-            }
-        }*/
 
         [HarmonyPatch(typeof(FVRFireArmRound), "UpdateInteraction")]
         [HarmonyPostfix]
         private static void BulletInteractionPatch(FVRFireArmRound __instance)
         {
-            if (!isStovepiping) return;
+            if (!_isStovepiping) return;
             if (!__instance.IsHeld) return;
             
             SetBulletBackToNormal();
@@ -237,7 +185,7 @@ namespace Stovepipe
         [HarmonyPostfix]
         private static void BulletDecayPatch(ref float ___m_killAfter)
         {
-            if (!isStovepiping) return;
+            if (!_isStovepiping) return;
 
             ___m_killAfter = 5f;
         }
@@ -246,7 +194,7 @@ namespace Stovepipe
         [HarmonyPrefix]
         private static bool AbortExtractingMagIfStovepiping()
         {
-            return !isStovepiping;
+            return !_isStovepiping;
         }
 
     }
